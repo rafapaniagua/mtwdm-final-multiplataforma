25f7a2888afa693a8b0da50e1141260e10e3c7a7
rafapaniagua <rafa_1_1@live.com.mx>
2023-05-03 02:18:04 -0600

    Ajuste de componente 'Banner', para que carrousel comience a girar las imágenes automáticamente

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="../assets/img/banner/slider1.png" class="d-block w-100" alt="Slider 1" >
@@ -14,11 +14,11 @@
         <img src="../assets/img/banner/slider4.jpeg" class="d-block w-100" alt="Slider 4" >
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
